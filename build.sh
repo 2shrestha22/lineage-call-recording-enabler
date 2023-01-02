726bea3dbf304ea03c020649eff3744f0f4f805c
Sangam Kumar Shrestha <2shrestha22@gmail.com>
2023-01-02 15:16:14 +0545

    Add Magisk module (#2)
    add magisk module

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
-7za a -tzip -r lineage-dialer-rro.zip ./flashable/*
-java -jar ./bin/zipsigner.jar lineage-dialer-rro.zip lineage-dialer-rro-signed.zip
+7za a -tzip -r lineage-dialer-rro_magisk.zip ./magisk/*
+7za a -tzip -r lineage-dialer-rro_recovery.zip ./recovery/*
+java -jar ./bin/zipsigner.jar lineage-dialer-rro_recovery.zip lineage-dialer-rro-signed_recovery.zip
